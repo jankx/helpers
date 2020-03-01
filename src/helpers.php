9fe5d82c6601f9aa8b4612261034e3d63b8c9e86
Puleeno Nguyen <puleeno@gmail.com>
2020-03-01 20:27:15 +0700

    Added array_get helper

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,15 @@
 <?php
+
+if (!function_exists('array_get')) {
+    function array_get($array, $key, $defaultValue = false)
+    {
+        $keys = explode('.', $key);
+        foreach ($keys as $key) {
+            if (!isset($array[$key])) {
+                return $defaultValue;
+            }
+            $value = $array = $array[$key];
+        }
+        return $value;
+    }
+}
